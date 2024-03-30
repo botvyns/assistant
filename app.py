68b4269be3e479e9155cae9a48513cb6f65ff2cc
botvyns <105560859+botvyns@users.noreply.github.com>
2024-03-30 16:16:29 +0200

    Add custom changes

--- app.py
+++ app.py
@@ -2,47 +2,43 @@ import time
 import streamlit as st
 from utils import load_chain
 
-# Custom image for the app icon and the assistant's avatar
-company_logo = 'https://blendle.com/img/packaging/android-touch-icon-196x196.png'
-
 # Configure streamlit page
 st.set_page_config(
-    page_title="Your Notion Chatbot",
-    page_icon=company_logo
+    page_title="Чатбот, що допоможе дізнатися більше про Neformat.com.ua"
 )
 
 # Initialize LLM chain in session_state
 if 'chain' not in st.session_state:
-    st.session_state['chain_loaded'] = True  # To avoid repeated loading attempts
+    # To avoid repeated loading attempts
+    st.session_state['chain_loaded'] = True
     st.session_state['chain'] = load_chain()
 
 # Initialize chat history
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant",
-                                     "content": "Hi human! I am Blendle's smart AI. How can I help you today?"}]
+                                     "content": "Привіт! Мене створили, щоб я допомагав тобі краще зрозуміти статтю про історію видання Neformat - твого гайду локальною сценою."}]
 
 # Display chat messages from history on app rerun
-# Custom avatar for the assistant, default avatar for user
 for message in st.session_state.get('messages', []):
     if message["role"] == 'assistant':
-        with st.chat_message(message["role"], avatar=company_logo):
+        with st.chat_message(message["role"]):
             st.markdown(message["content"])
     else:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 # Chat logic
-if query := st.chat_input("Ask me anything"):
+if query := st.chat_input("Запитай мене про Neformat.com.ua"):
     # Add user message to chat history
     st.session_state.setdefault('messages', []).append({"role": "user", "content": query})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(query)
 
-    with st.chat_message("assistant", avatar=company_logo):
+    with st.chat_message("assistant"):
         message_placeholder = st.empty()
-        # Send user's question to our chain
+        # Send user's question to chain
         result = st.session_state['chain']({"question": query})
         response = result['answer']
         full_response = ""
